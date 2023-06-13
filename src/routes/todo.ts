c768806e207b37f5155267f4542716ac021ac258
hugostanley <stanleyhugo06@gmail.com>
2023-06-13 16:17:28 +0800

    feat: add route for todo feed

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addNewTodo, deleteTodo, editTodo } from '../controllers/index.js'
+import { addNewTodo, deleteTodo, editTodo, getFeed } from '../controllers/index.js'
 import { authenticateUser } from "../middlewares/index.js"
 
 export const todoRouter: Router = Router()
 todoRouter.post("/todo/new", authenticateUser, addNewTodo)
 todoRouter.post("/todo/edit", authenticateUser, editTodo)
 todoRouter.post("/todo/delete", authenticateUser, deleteTodo)
+todoRouter.get("/todo/feed", authenticateUser, getFeed)
